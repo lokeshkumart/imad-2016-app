1dd0870508165bfe8fd6da63c1474781683a5f7b
lokeshkumart <ee16b040@smail.iitm.ac.in>
2016-10-15 12:02:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/article-one',function(req,res){
-res.send('This is my first url path');
+ res.sendFile(path.join(__dirname, 'ui', 'ui/article-one.html'));
 });
 
 app.get('/article-two',function(req,res){
